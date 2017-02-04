5b6e310e6fadae15a07e639151901112bb0be5c0
Matt Crowe <github.mpc@zym.me>
2017-02-04 14:38:16 +0000

    refactor front to web

--- resources/views/page/web/show.blade.php
+++ resources/views/page/web/show.blade.php
@@ -1,4 +1,4 @@
-@extends('ohio-core::layouts.front.main')
+@extends('ohio-core::layouts.web.main')
 
 @section('meta-title', $page->meta_title)
 @section('meta-description', $page->meta_description)
