c631dc0305371fd833b7c07cbe31554b9697feee
Matt Crowe <github.mpc@zym.me>
2017-02-13 21:33:50 +0000

    handleable responses route

---
+++ routes/handleables.php
@@ -0,0 +1,12 @@
+<?php
+
+use Ohio\Content\Http\Controllers\Web;
+
+# pages
+Response::macro('pages', function ($page) {
+
+    $controller = app()->make(Web\PagesController::class);
+    $response = $controller->callAction('show', ['page' => $page]);
+
+    return Response::make($response);
+});
