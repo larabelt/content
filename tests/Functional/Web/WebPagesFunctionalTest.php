af61d525ea2604860d32c282e2894866471ea0ae
Matt Crowe <github.mpc@zym.me>
2017-03-03 16:49:57 +0000

    tests

--- tests/Functional/Web/WebPagesFunctionalTest.php
+++ tests/Functional/Web/WebPagesFunctionalTest.php
@@ -6,32 +6,36 @@ use Belt\Core\User;
 class WebPagesFunctionalTest extends Testing\BeltTestCase
 {
 
-    public function test()
+    public function testAsSuper()
     {
         $this->refreshDB();
         $this->actAsSuper();
 
-////        # preview
-////        $response = $this->json('GET', '/pages/sectioned/preview');
-////        $response->assertStatus(200);
-////
-////        # show compiles b/c debug env
-////        putenv("APP_DEBUG=true");
-////        $response = $this->json('GET', '/pages/1');
-////        $response->assertStatus(200);
-//
-//        # show compiles b/c acting as super
-//        putenv("APP_DEBUG=false");
-//        $response = $this->json('GET', '/pages/1');
-//        $response->assertStatus(200);
-//
-//        # show caches b/c logged in user does not own page
-//        $this->actingAs(factory(User::class)->make(['is_super' => false]));
-//        $response = $this->json('GET', '/pages/1');
-//        s($response->content());
-//        $response->assertStatus(200);
+        # preview
+        $response = $this->json('GET', '/pages/sectioned/preview');
+        $response->assertStatus(200);
 
+        # show compiles b/c debug env
+        putenv("APP_DEBUG=true");
+        $response = $this->json('GET', '/pages/1');
+        $response->assertStatus(200);
 
+        # show compiles b/c acting as super
+        putenv("APP_DEBUG=false");
+        $response = $this->json('GET', '/pages/1');
+        $response->assertStatus(200);
+
+    }
+
+    public function testAsLoggedInUser()
+    {
+        $this->refreshDB();
+
+        # show caches b/c logged in user does not own page
+        putenv("APP_DEBUG=false");
+        $this->actingAs(factory(User::class)->make(['is_super' => false]));
+        $response = $this->json('GET', '/pages/1');
+        $response->assertStatus(200);
     }
 
 }
