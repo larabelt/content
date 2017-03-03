530f7b1177614f9ac791d1ec573b83c58b31b0e6
Matt Crowe <github.mpc@zym.me>
2017-03-03 17:04:55 +0000

    tout web show

--- resources/views/touts/web/show.blade.php
+++ resources/views/touts/web/show.blade.php
@@ -1,5 +1,7 @@
 <div class="well well-sm">
+    @if($tout->image)
     <img class="img-responsive" src="{{ $tout->image->src }}"/>
+    @endif
     <p><strong>{{ $tout->name }}</strong></p>
     <p>{!! $tout->body !!}</p>
     <p><a class="btn btn-primary" href="{{ $tout->btn_url }}">{{ $tout->btn_text }}</a></p>
