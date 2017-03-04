372ed69405abf66f8f48c00147dbbf3f4b03d635
Matt Crowe <github.mpc@zym.me>
2017-03-04 14:21:21 +0000

    add conditions to section templates

--- resources/views/blocks/sections/default.blade.php
+++ resources/views/blocks/sections/default.blade.php
@@ -2,9 +2,11 @@
     $block = $section->sectionable;
 @endphp
 
-<div class="section section-block {{ $section->param('class') }}">
-    @include('belt-content::sections.sections._heading')
-    @include('belt-content::sections.sections._before')
-    @include('belt-content::blocks.web.show')
-    @include('belt-content::sections.sections._after')
-</div>
+@if($block)
+    <div class="section section-block {{ $section->param('class') }}">
+        @include('belt-content::sections.sections._heading')
+        @include('belt-content::sections.sections._before')
+        @include('belt-content::blocks.web.show')
+        @include('belt-content::sections.sections._after')
+    </div>
+@endif
