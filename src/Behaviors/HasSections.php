2aad376664f1a4e6d5675e31fa6ad294b46f4edb
Matt Crowe <github.mpc@zym.me>
2017-03-28 15:27:47 +0000

    HasSections purge

--- src/Behaviors/HasSections.php
+++ src/Behaviors/HasSections.php
@@ -1,6 +1,7 @@
 <?php
 namespace Belt\Content\Behaviors;
 
+use DB;
 use Belt\Content\Section;
 
 trait HasSections
@@ -12,6 +13,15 @@ trait HasSections
     public function sections()
     {
         return $this->morphMany(Section::class, 'owner')->whereNull('parent_id')->orderBy('_lft');
+    }
+
+    public function purgeSections()
+    {
+        DB::connection($this->getConnectionName())
+            ->table('sections')
+            ->where('owner_id', $this->id)
+            ->where('owner_type', $this->getMorphClass())
+            ->delete();
     }
 
 }
