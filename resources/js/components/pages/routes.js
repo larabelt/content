64d14accaaa7d044d06ec26db9392e66bc4c2161
rob.lasota <rob.lasota@fahlgren.com>
2017-04-08 11:50:39 -0400

    adding attachments to categories

--- resources/js/components/pages/routes.js
+++ resources/js/components/pages/routes.js
@@ -1,6 +1,7 @@
 import index from './ctlr/index';
 import create from './ctlr/create';
 import edit  from './ctlr/edit';
+import attachments  from './ctlr/attachments';
 import categories  from './ctlr/categories';
 import handles  from './ctlr/handles';
 import tags  from './ctlr/tags';
@@ -10,6 +11,7 @@ export default [
     {path: '/pages', component: index, canReuse: false, name: 'pages'},
     {path: '/pages/create', component: create, name: 'pages.create'},
     {path: '/pages/edit/:id', component: edit, name: 'pages.edit'},
+    {path: '/pages/edit/:id/attachments', component: attachments, name: 'pages.attachments'},
     {path: '/pages/edit/:id/categories', component: categories, name: 'pages.categories'},
     {path: '/pages/edit/:id/handles', component: handles, name: 'pages.handles'},
     {path: '/pages/edit/:id/sections/:section?', component: sections, name: 'pages.sections'},
