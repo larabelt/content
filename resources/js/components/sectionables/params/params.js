f060fd3ab4878b18b4738070dfc240c2f10ef143
Matt Crowe <github.mpc@zym.me>
2017-04-24 00:19:33 +0000

    section.params bug

--- resources/js/components/sectionables/params/params.js
+++ resources/js/components/sectionables/params/params.js
@@ -11,7 +11,7 @@ export default {
     mixins: [shared],
     data() {
         let params = {};
-        for(let key in this.$parent.config.params) {
+        for (let key in this.$parent.config.params) {
             let form = new Form({section: this.section});
             form.setData({
                 key: key,
@@ -25,12 +25,13 @@ export default {
         }
     },
     created() {
-        let self = this;
-        self.table.index()
-            .then(function () {
-                for(let key in self.table.items) {
-                    let item = self.table.items[key];
-                    self.params[item.key].show(item.id);
+        this.table.index()
+            .then(() => {
+                for (let n in this.table.items) {
+                    let item = this.table.items[n];
+                    if (_.has(this.params, item.key)) {
+                        this.params[item.key].show(item.id);
+                    }
                 }
             });
     },
