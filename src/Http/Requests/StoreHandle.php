72eec1f603fe09d53402513f0af399afd5d1c6db
Matt Crowe <github.mpc@zym.me>
2017-08-28 21:10:50 +0000

    denote required fields in admin. remove various redudant html attributes

--- src/Http/Requests/StoreHandle.php
+++ src/Http/Requests/StoreHandle.php
@@ -20,14 +20,12 @@ class StoreHandle extends HandleFormRequest
                 'required',
                 'unique_route',
                 'unique:handles,url',
-                //'unique:handles,target',
             ],
             'handleable_id' => $this->config('show_handlable', false) ? 'required' : '',
             'handleable_type' => $this->config('show_handlable', false) ? 'required' : '',
         ];
 
         if ($this->config('show_target', false)) {
-            //$rules['target'] = 'required|unique:handles,url';
             $rules['target'] = 'required';
         }
 
--- src/Http/Requests/UpdateHandle.php
+++ src/Http/Requests/UpdateHandle.php
@@ -23,7 +23,6 @@ class UpdateHandle extends HandleFormRequest
             'url' => [
                 'sometimes',
                 'unique_route',
-                //'unique:handles,target',
                 Rule::unique('handles', 'url')->ignore($handle->id),
             ],
             'handleable_id' => $this->config('show_handlable', false) ? 'sometimes' : '',
@@ -31,7 +30,6 @@ class UpdateHandle extends HandleFormRequest
         ];
 
         if ($this->config('show_target', false)) {
-            //$rules['target'] = 'sometimes|unique:handles,url';
             $rules['target'] = 'required';
         }
 
