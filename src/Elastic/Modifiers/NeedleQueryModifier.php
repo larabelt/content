601b593a451ece4c2d7e978ad9bb9fb72ac96a41
Matt Crowe <github.mpc@zym.me>
2018-01-22 16:11:13 +0000

    elastic wildcard boost

--- src/Elastic/Modifiers/NeedleQueryModifier.php
+++ src/Elastic/Modifiers/NeedleQueryModifier.php
@@ -15,16 +15,36 @@ class NeedleQueryModifier extends PaginationQueryModifier
      */
     public function modify(PaginateRequest $request)
     {
-        if ($needle = $request->needle()) {
+        if ($needle = $this->needle($request)) {
             $this->engine->query['bool']['should'][]['multi_match'] = [
                 'query' => $needle,
                 'fields' => ['name^10', 'meta_title^5', 'meta_keywords^5', 'meta_description^5', 'searchable'],
                 'type' => 'best_fields',
                 'tie_breaker' => 0.3,
             ];
+            //$this->engine->query['bool']['should'][]['wildcard'] = [
+            //    'name' => "*$needle*",
+            //];
             $this->engine->query['bool']['should'][]['wildcard'] = [
-                'name' => "*$needle*",
+                'name' => [
+                    'wildcard' => "*$needle*",
+                    'boost' => 2 * strlen($needle),
+                ]
             ];
         }
+    }
+
+    /**
+     * @param PaginateRequest $request
+     * @return mixed|null|string|string[]
+     */
+    public function needle(PaginateRequest $request)
+    {
+        $needle = strtolower($request->needle());
+        $needle = str_replace(["'", '"'], ' ', urldecode($needle));
+        $needle = preg_replace('/\s+/', ' ', $needle);
+        $needle = preg_replace("/[^0-9a-zA-Z ]/", '', $needle);
+
+        return $needle;
     }
 }
