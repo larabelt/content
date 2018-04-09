715f2741ef0e26d92240be37910dae652c6acede
Matt Crowe <github.mpc@zym.me>
2018-04-09 21:57:32 +0000

    tweak seeds/migrations/factories

---
+++ config/templates/sections/containers/width-9.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'builder' => null,
+    'extends' => '',
+    'path' => 'belt-content::sections.sections.width-9',
+    'label' => '',
+    'description' => '',
+    'preview' => '',
+    'params' => [
+
+    ],
+];
