517d423803e498b859bd3b7b0d728be6b7c13871
Matt Crowe <github.mpc@zym.me>
2018-06-01 18:31:40 +0000

    list inputs

---
+++ resources/belt/content/js/inputs/lists/index.js
@@ -0,0 +1,38 @@
+import shared from 'belt/core/js/inputs/shared';
+import ListTable from 'belt/content/js/lists/table';
+import ListForm from 'belt/content/js/lists/form';
+import html from 'belt/content/js/inputs/lists/template.html';
+
+export default {
+    mixins: [shared],
+    data() {
+        return {
+            list: new ListForm(),
+            lists: new ListTable({query: {perPage: 20}}),
+        };
+    },
+    created() {
+        this.config.label = _.get(this.config, 'label', 'List');
+        this.config.description = _.get(this.config, 'description', 'Use the search field to find lists that can be linked to this item.');
+        this.$watch('form.' + this.column, function (newValue) {
+            this.list.show(newValue);
+        });
+    },
+    mounted() {
+        if (this.value) {
+            this.list.show(this.value);
+        }
+    },
+    methods: {
+        clear() {
+            this.lists.query.q = '';
+        },
+        update(id) {
+            this.form[this.column] = id;
+            this.clear();
+            this.emitEvent();
+        },
+    },
+    components: {},
+    template: html
+}
