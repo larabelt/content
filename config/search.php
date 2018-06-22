e4deb89f789414a64b017dff9439800cfb0d6d16
Matt Crowe <github.mpc@zym.me>
2018-06-22 17:32:47 +0000

    config default/example update

--- config/search.php
+++ config/search.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'default_engine' => env('SCOUT_DRIVER', 'local'),
     'classes' => [
         \Belt\Content\Page::class => \Belt\Content\Http\Requests\PaginatePages::class,
         \Belt\Content\Post::class => \Belt\Content\Http\Requests\PaginatePosts::class,
