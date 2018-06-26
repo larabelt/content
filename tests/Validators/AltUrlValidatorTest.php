aca1fa088dbefe77649a401a7616af650334a032
Matt Crowe <github.mpc@zym.me>
2018-06-26 16:01:55 +0000

    tests

--- tests/Validators/AltUrlValidatorTest.php
+++ tests/Validators/AltUrlValidatorTest.php
@@ -16,10 +16,7 @@ class AltUrlValidatorTest extends BeltTestCase
         $AltUrlValidator = new AltUrlValidator();
 
         $this->assertTrue($AltUrlValidator->altUrl('url', 'http://google.com', [], $validator));
-        /**
-         * @todo make this assert false again. local testing is returning a 200 response for http://localhost/foo, where it did not before.
-         */
-        //$this->assertFalse($AltUrlValidator->altUrl('url', 'foo', [], $validator));
+        $this->assertFalse($AltUrlValidator->altUrl('url', 'foo', [], $validator));
     }
 
 }
