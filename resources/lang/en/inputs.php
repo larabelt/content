4d7ff71a546ecd3a77be9cd85832e02fa19c2d6c
Matt Crowe <github.mpc@zym.me>
2018-09-17 19:31:34 +0000

    language/localization updates

---
+++ resources/lang/en/inputs.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'attachments' => [
+        'label' => 'Attachment',
+        'description' => 'The attachment needs to be uploaded first under the "Attachments" tab or "Attachments" section before linking it here.',
+    ]
+];
--- resources/lang/en/seo.php
+++ resources/lang/en/seo.php
@@ -3,5 +3,6 @@
 return [
     'editor' => [
         'overall' => 'Meta data is the key words and phrases that describe the contents of the page for search results and search engine indexing.',
+        'keywords' => 'Please format your keyword list with commas separating words. Ex. summer, beach, waves.',
     ]
 ];
