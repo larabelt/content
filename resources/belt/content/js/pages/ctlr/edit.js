6174fdffa767a56427e24ee664b873a2e433f3dd
Matt Crowe <github.mpc@zym.me>
2018-10-25 21:31:44 +0000

    continue translations

--- resources/belt/content/js/pages/ctlr/edit.js
+++ resources/belt/content/js/pages/ctlr/edit.js
@@ -1,25 +1,26 @@
 import shared from 'belt/content/js/pages/ctlr/shared';
-
+import TranslationStore from 'belt/core/js/translations/store/adapter';
 import form_html from 'belt/content/js/pages/templates/form.html';
 
 export default {
     mixins: [shared],
     components: {
         tab: {
+            mixins: [TranslationStore],
             data() {
                 return {
                     form: this.$parent.form,
                     entity_id: this.$parent.entity_id,
                 }
             },
+            created() {
+                this.bootTranslationStore();
+            },
             methods: {
                 submit() {
                     Events.$emit('pages:' + this.entity_id + ':updating', this.form);
                     this.form.submit();
                 }
-            },
-            components: {
-
             },
             template: form_html,
         },
