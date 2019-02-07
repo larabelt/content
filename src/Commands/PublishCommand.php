a02b5e6d705b3cf6dba9d85de5b836b503d59bae
Matt Crowe <github.mpc@zym.me>
2019-02-07 22:12:42 +0000

    continued unified documentation

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -33,7 +33,7 @@ class PublishCommand extends Command
         'vendor/larabelt/content/database/factories' => 'database/factories',
         'vendor/larabelt/content/database/migrations' => 'database/migrations',
         'vendor/larabelt/content/database/seeds' => 'database/seeds',
-        'vendor/larabelt/content/docs' => 'resources/docs',
+        'vendor/larabelt/content/docs' => 'resources/docs/raw',
     ];
 
 }
