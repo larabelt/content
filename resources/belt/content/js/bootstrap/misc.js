108932c6e7437a5c7e946c9aa28120492af219aa
Matt Crowe <github.mpc@zym.me>
2019-02-11 21:57:54 +0000

    add belt-clip to admin bootstrap

--- resources/belt/content/js/bootstrap/misc.js
+++ resources/belt/content/js/bootstrap/misc.js
@@ -1,3 +1,5 @@
 import AttachmentThumb from 'belt/content/js/attachments/thumb';
+import BeltVue from 'belt-vue-components';
 
 Vue.component('attachment-thumbnail', AttachmentThumb);
+Vue.component('belt-clip', BeltVue.BeltClip);
