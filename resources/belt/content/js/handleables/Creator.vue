0e77f3ee70164258861bf75288a36968352c0ebd
Matt Crowe <github.mpc@zym.me>
2019-02-11 18:07:45 +0000

    fix handleable manager tab bug

--- resources/belt/content/js/handleables/Creator.vue
+++ resources/belt/content/js/handleables/Creator.vue
@@ -2,26 +2,26 @@
     <div>
         <h4>Create New Handle</h4>
         <p class="help-block">Use the field to add a new handle to this item.</p>
-        <form @submit.prevent="store()" @keydown="form.errors.clear($event.target.name)" style="margin-bottom: 20px;" class="form-inline">
+        <form @submit.prevent="store()" @keydown="createForm.errors.clear($event.target.name)" style="margin-bottom: 20px;" class="form-inline">
             <div v-if="hasLocales" class="form-group">
                 <input-locale
-                        :form="form"
+                        :form="createForm"
                 ></input-locale>
             </div>
             <div class="form-group">
                 <div class="input-group">
-                    <input type="name" class="form-control" v-model="form.url" placeholder="enter handle url">
+                    <input type="name" class="form-control" v-model="createForm.url" placeholder="enter handle url">
                     <span class="input-group-btn">
-                        <button class="btn btn-primary" :disabled="form.errors.any()">
-                            <span v-if="form.saving"><i class="fa fa-spinner fa-pulse fa-fw"></i></span>
+                        <button class="btn btn-primary" :disabled="createForm.errors.any()">
+                            <span v-if="createForm.saving"><i class="fa fa-spinner fa-pulse fa-fw"></i></span>
                             <span v-else>add</span>
                         </button>
                     </span>
                 </div>
             </div>
         </form>
-        <div class="form-group" :class="{ 'has-error': form.error('url') }">
-            <span v-for="error in form.error('url')" class="text-danger">{{ error }}</span>
+        <div class="form-group" :class="{ 'has-error': createForm.error('url') }">
+            <span v-for="error in createForm.error('url')" class="text-danger">{{ error }}</span>
         </div>
     </div>
 </template>
@@ -39,22 +39,22 @@
         },
         data() {
             return {
-                form: new Form(),
+                createForm: new Form(),
             }
         },
         created() {
-            this.setForm();
+            this.setCreateForm();
         },
         methods: {
-            setForm() {
-                this.form = new Form({entity_type: this.handleableType, entity_id: this.handleableID});
+            setCreateForm() {
+                this.createForm = new Form({entity_type: this.handleableType, entity_id: this.handleableID});
             },
             store() {
-                this.form.store()
+                this.createForm.store()
                     .then(() => {
-                        let handle = this.form;
+                        let handle = this.createForm;
                         this.pushHandles([handle]);
-                        this.setForm();
+                        this.setCreateForm();
                     })
             },
         },
