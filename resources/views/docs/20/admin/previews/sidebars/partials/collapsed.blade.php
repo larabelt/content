ba40c1ac3c74cc3475a2ee437a56da58ea46f4d0
Matt Crowe <github.mpc@zym.me>
2019-02-12 21:50:13 +0000

    continue unified documentation

---
+++ resources/views/docs/20/admin/previews/sidebars/partials/collapsed.blade.php
@@ -0,0 +1,15 @@
+<li id="content-admin-sidebar-left" class="treeview">
+    <a href="#">
+        <i class="fa fa-file-o"></i> <span>Content</span> <i class="fa fa-angle-left pull-right"></i>
+    </a>
+    <ul class="treeview-menu">
+        <li id="content-admin-sidebar-left-attachments"><a href="/admin/belt/content/attachments"><i class="fa fa-paperclip"></i> <span>Attachments</span></a></li>
+        <li id="content-admin-sidebar-left-blocks"><a href="/admin/belt/content/blocks"><i class="fa fa-code"></i> <span>Blocks</span></a></li>
+        <li id="content-admin-sidebar-left-handles"><a href="/admin/belt/content/handles?orderBy=-handles.hits&is_active=0"><i class="fa fa-signing"></i> <span>Handles</span></a></li>
+        <li id="content-admin-sidebar-left-lists"><a href="/admin/belt/content/lists"><i class="fa fa-list"></i> <span>Lists</span></a></li>
+        <li id="content-admin-sidebar-left-pages"><a href="/admin/belt/content/pages"><i class="fa fa-files-o"></i> <span>Pages</span></a></li>
+        <li id="content-admin-sidebar-left-posts"><a href="/admin/belt/content/posts"><i class="fa fa-thumb-tack"></i> <span>Posts</span></a></li>
+        <li id="content-admin-sidebar-left-terms"><a href="/admin/belt/content/terms"><i class="fa fa-sitemap"></i> <span>Terms</span></a></li>
+        <li id="content-admin-sidebar-left-terms"><a href="/admin/belt/content/translatable-strings"><i class="fa fa-keyboard-o"></i> <span>Strings</span></a></li>
+    </ul>
+</li>
