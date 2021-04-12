07a7da60d12f06a0ce8222093db8a3d8af5fc095
rob.lasota <rob.lasota@gmail.com>
2021-04-12 12:08:11 -0400

    posts inputs

--- resources/belt/content/js/bootstrap/inputs.js
+++ resources/belt/content/js/bootstrap/inputs.js
@@ -2,7 +2,7 @@ import attachments from 'belt/content/js/inputs/attachments';
 import blocks from 'belt/content/js/inputs/blocks';
 import lists from 'belt/content/js/inputs/lists';
 import pages from 'belt/content/js/inputs/pages';
-import pages from 'belt/content/js/inputs/posts';
+import posts from 'belt/content/js/inputs/posts';
 import terms from 'belt/content/js/inputs/terms';
 import seo from 'belt/content/js/inputs/seo';
 
@@ -10,6 +10,6 @@ Vue.component('input-attachments', attachments);
 Vue.component('input-blocks', blocks);
 Vue.component('input-lists', lists);
 Vue.component('input-pages', pages);
-Vue.component('input-posts', pages);
+Vue.component('input-posts', posts);
 Vue.component('input-terms', terms);
 Vue.component('input-seo', seo);
