2219694bca4c21049ad968ca1bf11a305910ee94
Matt Crowe <github.mpc@zym.me>
2021-04-30 00:01:56 +0000

    Move searchable build/save to Observer from CompileService.

--- src/Observers/HasSectionsObserver.php
+++ src/Observers/HasSectionsObserver.php
@@ -3,9 +3,20 @@
 namespace Belt\Content\Observers;
 
 use Belt\Content\Behaviors\HasSectionsInterface;
-
+use Html2Text\Html2Text;
 class HasSectionsObserver
 {
+    /**
+     * Listen to the Handle saving $handle.
+     *
+     * @handle  Handle $handle
+     * @return void
+     */
+    public function saving(HasSectionsInterface $item)
+    {
+        $item->searchable = $this->crawl($item);
+    }
+    
     /**
      * Listen to the HasSectionsInterface deleting $item.
      *
@@ -15,5 +26,73 @@ class HasSectionsObserver
     public function deleting(HasSectionsInterface $item)
     {
         $item->sections()->delete();
+    }
+
+    /**
+     * @param HasSectionsInterface $owner
+     *
+     * @return string
+     */
+    private function crawl(HasSectionsInterface $item)
+    {
+
+        $searchable = $this->getSearchable($item);
+
+        foreach ($item->sections as $section) {
+            $searchable = $this->__crawl($section, $searchable);
+        }
+
+        $searchable = preg_replace('/\s+/', ' ', $searchable);
+
+        try {
+            $text = Html2Text::convert($searchable);
+            $searchable = $text;
+        } catch (\Exception $e) {
+
+        }
+
+        return $searchable;
+    }
+
+    /**
+     * @param $section
+     * @param $searchable
+     *
+     * @return string
+     */
+    private function __crawl($section, $searchable = '')
+    {
+
+        $searchable = $this->getSearchable($section, $searchable);
+
+        foreach ($section->children as $child) {
+            $searchable = $this->__crawl($child, $searchable);
+        }
+
+        return $searchable;
+    }
+
+    /**
+     * @param $item
+     * @param string $searchable
+     * @return string
+     */
+    private function getSearchable($item, $searchable = '')
+    {
+        $new = [];
+        $new[] = $item->getAttribute('name');
+        $new[] = $item->getAttribute('heading');
+        $new[] = $item->getAttribute('before');
+        $new[] = $item->getAttribute('body');
+        $new[] = $item->getAttribute('after');
+        $new[] = $item->getAttribute('meta_title');
+        $new[] = $item->getAttribute('meta_keywords');
+        $new[] = $item->getAttribute('meta_description');
+
+        $new = preg_replace('/\s+/', ' ', implode(' ', $new));
+
+        $searchable .= strip_tags($new);
+
+        return $searchable;
     }
 }
--- src/Services/CompileService.php
+++ src/Services/CompileService.php
@@ -4,7 +4,6 @@ namespace Belt\Content\Services;
 
 use Belt, Cache, View;
 use Belt\Content\Behaviors\HasSectionsInterface;
-use Html2Text\Html2Text;
 
 /**
  * Class CompileService
@@ -25,8 +24,7 @@ class CompileService
 
         try {
             $compiled = View::make($owner->subtype_view, ['owner' => $owner])->render();
-            $owner->searchable = $this->crawl($owner);
-            $owner->save();
+            
         } catch (\Exception $e) {
 
         }
@@ -74,80 +72,6 @@ class CompileService
     public function getCached(HasSectionsInterface $owner)
     {
         return Cache::get($owner->getHasSectionsCacheKey()) ?: $this->compile($owner);
-    }
-
-    /**
-     * @param HasSectionsInterface $owner
-     *
-     * @return string
-     */
-    public function crawl(HasSectionsInterface $owner)
-    {
-
-        $searchable = $this->getSearchable($owner);
-
-        foreach ($owner->sections as $section) {
-            $searchable = $this->__crawl($section, $searchable);
-        }
-
-        $searchable = preg_replace('/\s+/', ' ', $searchable);
-
-        try {
-            $text = Html2Text::convert($searchable);
-            $searchable = $text;
-        } catch (\Exception $e) {
-
-        }
-
-        return $searchable;
-    }
-
-    /**
-     * @param $section
-     * @param $searchable
-     *
-     * @return string
-     */
-    public function __crawl($section, $searchable = '')
-    {
-
-        $searchable = $this->getSearchable($section, $searchable);
-
-//        if ($sectionable = $section->sectionable) {
-//            if ($sectionable instanceof SectionableInterface) {
-//                $searchable = $this->getSearchable($sectionable, $searchable);
-//            }
-//        }
-
-        foreach ($section->children as $child) {
-            $searchable = $this->__crawl($child, $searchable);
-        }
-
-        return $searchable;
-    }
-
-    /**
-     * @param $item
-     * @param string $searchable
-     * @return string
-     */
-    public function getSearchable($item, $searchable = '')
-    {
-        $new = [];
-        $new[] = $item->getAttribute('name');
-        $new[] = $item->getAttribute('heading');
-        $new[] = $item->getAttribute('before');
-        $new[] = $item->getAttribute('body');
-        $new[] = $item->getAttribute('after');
-        $new[] = $item->getAttribute('meta_title');
-        $new[] = $item->getAttribute('meta_keywords');
-        $new[] = $item->getAttribute('meta_description');
-
-        $new = preg_replace('/\s+/', ' ', implode(' ', $new));
-
-        $searchable .= strip_tags($new);
-
-        return $searchable;
     }
 
 }
